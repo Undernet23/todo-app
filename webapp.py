669db7a5748ba156e59821d0d86f906eee80639d
Marian <dobrea.marian.23@gmail.com>
2025-03-09 11:50:00 +0200

    Implement "Add new to-do items" feature

--- webapp.py
+++ webapp.py
@@ -2,6 +2,10 @@ import streamlit as st
 import  functions
 
 todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
 
 st.title("My to-do app.")
 st.subheader("This is my to-do app.")
@@ -13,4 +17,5 @@ for todo in todos:
     incrementor = incrementor + 1
 
 
-st.text_input(label="" ,placeholder="Add new to-do...")
+st.text_input(label=" " ,placeholder="Add new to-do...",
+              on_change=add_todo, key='new_todo')
